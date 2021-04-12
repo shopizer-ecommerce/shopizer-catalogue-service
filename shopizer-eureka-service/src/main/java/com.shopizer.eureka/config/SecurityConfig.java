54fe21e89a4afe76295f4dd7daf5606303086549
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-04-12 22:38:02 +0200

    Removed readme file

--- shopizer-eureka-service/src/main/java/com.shopizer.eureka/config/SecurityConfig.java
+++ shopizer-eureka-service/src/main/java/com.shopizer.eureka/config/SecurityConfig.java
@@ -1,7 +1,6 @@
-package guru.sfg.brewery.eureka.msscbreweryeureka.config;
+package com.shopizer.eureka.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
